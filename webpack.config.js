054b911cb5154aac76ebd37f76c7d407a7071ab5
Marco Brack <marco@glints.com>
2020-07-14 23:57:18 +0000

    Initialize with existing work

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src"),
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "webpack-typescript-babel",
+    libraryTarget: "umd",
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"],
+  },
+
+  module: {
+    rules: [
+      { test: /\.(ts|js)x?$/, loader: "babel-loader", exclude: /node_modules/ },
+    ],
+  },
+
+  plugins: [new ForkTsCheckerWebpackPlugin()],
+
+  externals: {
+    react: "react",
+    "react-dom": "ReactDOM",
+  },
+};
